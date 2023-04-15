0c64b37e637ffc439a358d2a2f6f35fa1b8b5df9
AlvaroBarry <121524021+AlvaroBarry@users.noreply.github.com>
2023-04-15 18:03:22 +0200

    second draft

---
+++ include/diffdrive_arduino/config.h
@@ -0,0 +1,21 @@
+#ifndef DIFFDRIVE_ARDUINO_CONFIG_H
+#define DIFFDRIVE_ARDUINO_CONFIG_H
+
+#include <string>
+
+
+struct Config
+{
+  std::string left_wheel_name = "left_wheel";
+  std::string right_wheel_name = "right_wheel";
+  std::string left_wheel_t_name = "left_wheel_t";
+  std::string right_wheel_t_name = "right_wheel_t";
+  float loop_rate = 30;
+  std::string device = "/dev/ttyACM0";
+  int baud_rate = 57600;
+  int timeout = 1000;
+  int enc_counts_per_rev = 1920;
+};
+
+
+#endif // DIFFDRIVE_ARDUINO_CONFIG_H
