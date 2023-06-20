1791d14aa1f1a10d5f7e5f9d0e4c8c8f33d7d4b1
AlvaroBarry <abarrigonfer@gmail.com>
2023-06-20 14:04:11 +0200

    baud rate change

--- include/diffdrive_arduino/config.h
+++ include/diffdrive_arduino/config.h
@@ -12,7 +12,7 @@ struct Config
   std::string right_wheel_t_name = "right_wheel_t";
   float loop_rate = 30;
   std::string device = "/dev/ttyUSB0";
-  int baud_rate = 57600;
+  int baud_rate = 115200;
   int timeout = 1000;
   int enc_counts_per_rev = 1920;
 };
